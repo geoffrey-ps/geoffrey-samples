19eef2638da5a1a8999879142248bb682506af24
Mads Kristensen <post@madskristensen.net>
2015-09-09 15:51:41 -0700

    updates

--- less-compiling/g.ps1
+++ less-compiling/g.ps1
@@ -1,7 +1,11 @@
 requires geoffrey-less
 
+# From Powershell console, call this using "geoffrey -taskName less"
 task less {
     src "less/site.less" |
         less |
         dest "output/site.css"
 }
+
+# Default tasks can be invoked simply by calling "geoffrey" in the PowerShell console
+task default -dependsOn less, less
