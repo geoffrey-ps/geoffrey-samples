e95b15c2440f342d2894371dbc3b44eaf968e6d2
Mads Kristensen <post@madskristensen.net>
2015-09-09 16:28:40 -0700

    New bundling sample

---
+++ bundling-minification/g.ps1
@@ -0,0 +1,20 @@
+requires geoffrey-less
+
+# From Powershell console, call this using "geoffrey -taskName css".
+task css {
+    dir "css/*.css" |
+        src |
+        cssmin -CommentMode Hacks |
+        dest "output/combined.css"
+}
+
+# From Powershell console, call this using "geoffrey -taskName js".
+task js {
+        src "js/a.js", "js/b.js" |
+        jsmin -MinifyCode 1 |
+        dest "output/combined.js"
+}
+
+# Default tasks can be invoked simply by calling "geoffrey" in the PowerShell console.
+# The "default" task will run both the "css" and "js" tasks when invoked.
+task default -dependsOn css, js
--- less-compiling/g.ps1
+++ less-compiling/g.ps1
@@ -1,11 +1,12 @@
 requires geoffrey-less
 
-# From Powershell console, call this using "geoffrey -taskName less"
+# From Powershell console, call this using "geoffrey -taskName less".
 task less {
     src "less/site.less" |
         less |
         dest "output/site.css"
 }
 
-# Default tasks can be invoked simply by calling "geoffrey" in the PowerShell console
-task default -dependsOn less, less
+# Default tasks can be invoked simply by calling "geoffrey" in the PowerShell console.
+# The "default" task will run both the "css" and "js" tasks when invoked.
+task default -dependsOn less
